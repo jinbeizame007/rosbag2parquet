90dd0cd3e5d49bd850192ec6fb9b9f9d115340e0
jinbeizame007 <jinbeizame007@gmail.com>
2025-07-23 14:40:35 +0900

    [add] read_mcap()

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs;
+
+use anyhow::{Context, Result};
+use camino::Utf8Path;
+
+use memmap2::Mmap;
+
+fn read_mcap<P: AsRef<Utf8Path>>(path: P) -> Result<Mmap> {
+    let fd = fs::File::open(path.as_ref()).context("Couldn't open MCap file")?;
+    unsafe { Mmap::map(&fd) }.context("Couldn't map MCap file")
 }
 
 #[cfg(test)]
@@ -7,8 +15,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_read_mcap() {
+        let path = "testdata/demo.mcap";
+        let mmap = read_mcap(path).unwrap();
+        assert!(!mmap.is_empty());
     }
 }
