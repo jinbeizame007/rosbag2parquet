cac80115b6810eec0fd5688d42ff02d0a9afe6d8
jinbeizame007 <jinbeizame007@gmail.com>
2025-08-11 17:44:36 +0900

    [add] list topic types in the test

--- src/lib.rs
+++ src/lib.rs
@@ -663,8 +663,19 @@ mod tests {
     #[test]
     fn test_record_batch_builder_large() {
         let mut topic_names = HashSet::new();
-        topic_names.insert("sensor_msgs/msg/JointState".to_string());
-        // topic_names.insert("/backhoe/hardware/cabin_orientation".to_string());
+        // topic_names.insert("sensor_msgs/msg/JointState".to_string());
+        // topic_names.insert("geometry_msgs/msg/QuaternionStamped".to_string());
+        // topic_names.insert("sensor_msgs/msg/PointCloud2".to_string());  // To be supported
+        // topic_names.insert("geometry_msgs/msg/PointStamped".to_string());
+        // topic_names.insert("std_msgs/msg/String".to_string());
+        // topic_names.insert("std_msgs/msg/Float64".to_string());
+        // topic_names.insert("geometry_msgs/msg/TwistStamped".to_string());
+        // topic_names.insert("sensor_msgs/msg/Image".to_string());
+        // topic_names.insert("visualization_msgs/msg/MarkerArray".to_string());
+        // topic_names.insert("nav_msgs/msg/OccupancyGrid".to_string());
+        // topic_names.insert("diagnostic_msgs/msg/DiagnosticArray".to_string());  // To be supported
+        // topic_names.insert("diagnostic_msgs/msg/DiagnosticStatus".to_string());  // To be supported
+        topic_names.insert("tf2_msgs/msg/TFMessage".to_string()); // To be supported
 
         let test_path = "rosbags/large2/large2.mcap";
         let record_batches =
