a6b7d0841ce3434368618734d9caa883c219acdd
jinbeizame007 <jinbeizame007@gmail.com>
2025-08-15 20:08:53 +0900

    [add] example

---
+++ examples/rosbag2parquet.rs
@@ -0,0 +1,27 @@
+use rosbag2parquet::{
+    rosbag2record_batches_optimized, rosbag2record_batches_with_topic_names,
+    write_record_batches_to_parquet,
+};
+use std::collections::HashSet;
+
+fn main() {
+    let mut topic_names = HashSet::new();
+    // topic_names.insert("sensor_msgs/msg/JointState".to_string());
+    // topic_names.insert("geometry_msgs/msg/QuaternionStamped".to_string());
+    // topic_names.insert("sensor_msgs/msg/PointCloud2".to_string());
+    // topic_names.insert("geometry_msgs/msg/PointStamped".to_string());
+    // topic_names.insert("std_msgs/msg/String".to_string());
+    // topic_names.insert("std_msgs/msg/Float64".to_string());
+    // topic_names.insert("geometry_msgs/msg/TwistStamped".to_string());
+    topic_names.insert("sensor_msgs/msg/Image".to_string());
+    // topic_names.insert("visualization_msgs/msg/MarkerArray".to_string());
+    // topic_names.insert("nav_msgs/msg/OccupancyGrid".to_string());
+    // topic_names.insert("diagnostic_msgs/msg/DiagnosticArray".to_string());
+    // topic_names.insert("diagnostic_msgs/msg/DiagnosticStatus".to_string());
+    // topic_names.insert("tf2_msgs/msg/TFMessage".to_string());
+
+    let test_path = "rosbags/large2/large2.mcap";
+    // let record_batches = rosbag2record_batches_with_topic_names(test_path, topic_names).unwrap();
+    let record_batches = rosbag2record_batches_optimized(test_path, Some(topic_names)).unwrap();
+    write_record_batches_to_parquet(record_batches, "rosbags/large2");
+}
